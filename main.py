df026d0678bd60043981696cb015eeda5ad9b975
Ivangabriel21210 <ivangabriel21210@gmail.com>
2024-09-28 20:54:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from os import system
 
 system("wget https://raw.githubusercontent.com/Ivangabriel21210/HolicraftDocker/refs/heads/main/gotty")
 system("chmod +x gotty")
-system("./gotty -w --port 8080 /bin/bash")
+system("./gotty -w --port 4040 /bin/bash")
