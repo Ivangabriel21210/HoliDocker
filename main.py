2bdbf648d6d1b7e20e3e2565e8868338821422eb
Ivangabriel21210 <ivangabriel21210@gmail.com>
2024-09-28 21:03:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from os import system
 
 system("wget https://raw.githubusercontent.com/Ivangabriel21210/HolicraftDocker/refs/heads/main/gotty")
 system("chmod +x gotty")
-system("./gotty -w --port 9283 /bin/bash")
+system("curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb")
+system("sudo dpkg -i cloudflared.deb")
+system("sudo cloudflared service install eyJhIjoiYzQ4OTA0MWRjOTNlZTM1YTE4ZWRhYTBhMDBiOWIzNDEiLCJ0IjoiZDRhOTYxZTEtMjVhYi00NTY1LWFhNGMtZDcxM2Y2MWYxY2IzIiwicyI6IlpEVTFaVGswWmpFdE5HRTRZUzAwT0Rka0xXRm1PR1V0TkdZd01UUTFPV1l5TUdFNSJ9")
 system("while :; do :; done")
